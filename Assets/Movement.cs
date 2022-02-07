70a1ddb3b9ca67d9083e37eb507cf1bf53c4440c
Abbebabbe124 <thecookiemaster60@gmail.com>
2022-02-07 10:48:13 +0100

    Movement script :D

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Rigidbody2D rb;
+
+    float horizontal;
+    float vertical;
+
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        //gayass
-        Debug.Log("Jag är inte gay");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // test
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+ void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) //Check for diagonal movement
+        {
+            //Limits movement speed diagonally
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        rb.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform myTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(myTarget.position.x, myTarget.position.y, gameObject.transform.position.z);
+    }
+}
