13472b13e015975a3877a90da6b290328daa6234
WarpABoi <alfkra@outlook.com>
2022-02-07 10:31:30 +0100

    Items
    - placeholder items
    -  inventory manager script

---
+++ Assets/Item Assets/ItemList.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemList : MonoBehaviour
+{
+    public enum Items
+    {
+        Empty, // dont touch or invenetory breaks
+        // add any items ater here
+        Apple,
+        Banana,
+        Orange
+    }
+}
