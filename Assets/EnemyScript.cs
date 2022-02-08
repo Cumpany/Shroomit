1fb8ab3fc102654b6ba6cbe08b24cef66ee2df15
Abbebabbe124 <thecookiemaster60@gmail.com>
2022-02-08 14:08:14 +0100

    enemy ian :DDD

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -6,6 +6,7 @@ public class EnemyScript : MonoBehaviour
 {
     [SerializeField] private GameObject Loot;
     public float enemyHP = 10;
+
     // Start is called before the first frame update
     void Start()
     {
