b86e845db56de78f715c5025366a9c15a566aeae
Abbebabbe124 <thecookiemaster60@gmail.com>
2022-02-09 09:55:23 +0100

    sakjlf

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -11,6 +11,8 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private string StartScene = "Start Scene";
     [SerializeField] private string OptionScene = "Option Scene";
     static string LastScene;
+
+    public AudioSource audioSource;
     public void StartGame()
     {
         LastScene = SceneManager.GetActiveScene().name;
@@ -33,5 +35,10 @@ public class MainMenu : MonoBehaviour
     public void VolumeSlider()
     {
         GlobalVolume = GameObject.Find("VolumeSlider").transform.GetComponent<Slider>().value;
+    }
+
+    public void SoundTest()
+    {
+        audioSource.Play();
     }
 }
