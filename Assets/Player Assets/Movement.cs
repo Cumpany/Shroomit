2ed4163acf51045b00863dc31baad2ba25ef90e1
Abbebabbe124 <thecookiemaster60@gmail.com>
2022-02-09 12:52:42 +0100

    Summary (required)
    Description

--- Assets/Player Assets/Movement.cs
+++ Assets/Player Assets/Movement.cs
@@ -9,6 +9,7 @@ public class Movement : MonoBehaviour
 
     float horizontal;
     float vertical;
+    float moveLimiter = 0.7f;
 
     public float runSpeed = 10.0f;
 
@@ -55,6 +56,12 @@ public class Movement : MonoBehaviour
 
  void FixedUpdate()
     {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
         rb.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 epic
 **﷽**
 .
-OM DU LÄSER DETTA ÄR DU GAY
+OM DU LÄSER DETTA ÄR DU GAYy_D!½½½½§§´´3q+''¨.a<<<		<	o
