5ff7ad569a18c1e7ce932f2163a188637b3c3018
WarpABoi <alfkra@outlook.com>
2022-02-10 09:05:09 +0100

    renamed inventory thingies for clarity

--- Assets/ItemAssets/PlayerInventory.cs
+++ Assets/ItemAssets/PlayerInventory.cs
@@ -7,29 +7,29 @@ using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour
 {
-    public ItemList.Items[] Inv = new ItemList.Items[9];
+    public ItemList.Items[] Inventory = new ItemList.Items[9];
     private ItemList.Items Cursor = new ItemList.Items();
-    [SerializeField] private GameObject Inventory;
+    [SerializeField] private GameObject InventoryObject;
     [SerializeField]private GameObject[] InvSlots = new GameObject[9];
     
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab) || Input.GetKeyDown(KeyCode.I))
         {
-            Inventory.SetActive(!Inventory.activeSelf);
+            InventoryObject.SetActive(!InventoryObject.activeSelf);
             DrawInv();
         }
     }
     void Start()
     {
         DrawInv();
-        Inventory.SetActive(false);
+        InventoryObject.SetActive(false);
     }
     void DrawInv()
     {
         for (var x = 0; x < 9; x++)
         {
-            switch ((int)Inv[x])
+            switch ((int)Inventory[x])
             {
                 case 0:
                 // Debug.Log($"case 0 at {x}");
@@ -87,14 +87,14 @@ public class PlayerInventory : MonoBehaviour
     {
         if (IsSlotFree(x))
         {
-            Inv[x] = i;
+            Inventory[x] = i;
             return true;
         }
         return false;
     }
     public void CursorToInv(int x)
     {
-        if (Cursor != 0 && Inv[x] != 0)
+        if (Cursor != 0 && Inventory[x] != 0)
         {
             AddItem(x,Cursor);
             Cursor = 0;
@@ -102,15 +102,15 @@ public class PlayerInventory : MonoBehaviour
     }
     public void InvToCursor(int x)
     {
-        if (Cursor != 0 && Inv[x] != 0)
+        if (Cursor != 0 && Inventory[x] != 0)
         {
-            Cursor = Inv[x];
-            Inv[x] = 0;
+            Cursor = Inventory[x];
+            Inventory[x] = 0;
         }
     }
     public bool IsSlotFree(int x)
     {
-        if (Inv[x] == 0)
+        if (Inventory[x] == 0)
         {
             return true;
         }
@@ -120,7 +120,7 @@ public class PlayerInventory : MonoBehaviour
     {
         for (var x = 0; x < 9; x++)
         {
-            Inv[x] = 0;
+            Inventory[x] = 0;
         }
     }
 }
