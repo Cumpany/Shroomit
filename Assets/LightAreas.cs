39931c147cac5eeec26e5526efb47a7833deb0f8
Abbebabbe124 <thecookiemaster60@gmail.com>
2022-02-16 10:32:23 +0100

    mörk skog sak fix :+1:

--- Assets/LightAreas.cs
+++ Assets/LightAreas.cs
@@ -1,25 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DarkForest : MonoBehaviour
+public class LightAreas : MonoBehaviour
 {
-    // the image you want to fade, assign in inspector
+    int x;
+
     public Image img;
-
-    int x = 1;
-
+    // Start is called before the first frame update
     void Start()
     {
         img.color = new Color(0, 0.042f, 0.34f, 0);
     }
+
     public void OnTriggerEnter2D(Collider2D col)
     {
         // fades the image out when you click
         if (col.gameObject.tag == "Player")
         {
-            StartCoroutine(FadeImage(false));
+            StartCoroutine(FadeImage(true));
         }
     }
 
@@ -27,10 +27,10 @@ public class DarkForest : MonoBehaviour
     {
         x++;
         // fade from opaque to transparent
-        if (x % 2 == 0 && !fadeAway)
+        if (x % 2 == 0 && fadeAway)
         {
             // loop over 1 second backwards
-            for (float i = 0; i <= 0.5; i += Time.deltaTime)
+            for (float i = 0.5f; i >= 0; i -= Time.deltaTime)
             {
                 // set color with i as alpha
                 img.color = new Color(0, 0.042f, 0.34f, i);
