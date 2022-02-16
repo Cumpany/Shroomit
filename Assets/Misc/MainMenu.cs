d601b56399144ba2e1cd9833487148acc1ff6e26
WarpABoi <alfkra@outlook.com>
2022-02-16 23:52:31 +0100

    fixed blank names

--- Assets/Misc/MainMenu.cs
+++ Assets/Misc/MainMenu.cs
@@ -26,6 +26,10 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
+        if (PlayerName == "")
+        {
+            PlayerName = "Anonymous";
+        }
         LastScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(StartScene);
     }
