d11bf7cc86fe4b280e907d4ce684b560530837d8
WarpABoi <alfkra@outlook.com>
2022-02-17 09:07:35 +0100

    Update MainMenu.cs

--- Assets/Misc/MainMenu.cs
+++ Assets/Misc/MainMenu.cs
@@ -26,7 +26,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        if (PlayerName == "")
+        if (string.IsNullOrEmpty(PlayerName))
         {
             PlayerName = "Anonymous";
         }
