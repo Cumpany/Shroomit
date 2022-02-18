8b2c7820af937d21be0dab6842b093f9a87b7b48
WarpABoi <alfkra@outlook.com>
2022-02-18 09:09:24 +0100

    Update SaveManager.cs

--- Assets/Misc/SaveManager.cs
+++ Assets/Misc/SaveManager.cs
@@ -220,5 +220,6 @@ public class SaveManager : MonoBehaviour
         }
         Debug.Log("deleted " + i.Length + "files");
         boss.enemyHP = boss.maxEnemyHP;
+        PlayerScript.health = PlayerScript.maxHealth;
     }
 }
